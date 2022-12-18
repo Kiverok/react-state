f892f23d1e5b31b7aca0321a7e03ceb97d24d770
Kiverok <kiver566@ukr.net>
2022-12-18 10:28:30 +0200

    select

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
+import { fetchDogByBreed } from "./api";
 import { BreedSelect } from "./components/BreedSelect";
 
 export class App extends Component {
   state = {
-    selectedBreed: null,
+    dog: null,
+    isLoading: false,
+    error: null,
   };
 
-  selectBreed = breedId => {
-    console.log(breedId)
-  }
+  selectBreed = async breedId => {
+    try {
+      this.setState({isLoading: true})
+      const dog = await fetchDogByBreed(breedId);
+      this.setState({ dog });
+    } catch {
+      this.setState({
+        error: "Помилка, попробуйте ще раз!"
+      })
+    } finally {
+      this.setState({ isLoading: false});
+    }
+  };
   render() {
+    const { error, dog, isLoading } = this.state;
   return (
     <div className="App">
       <BreedSelect onSelect={this.selectBreed} />
+      {dog && <img src={this.state.dog.url} alt="" />}
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {isLoading && <div>LOADING...</div>}
     </div>
   )}
 }
